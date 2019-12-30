af8c4c8c989dc33d348852a64d7bb7bf82897dcb
Ycode-Pro <59374054+Ycode-Pro@users.noreply.github.com>
2019-12-30 22:25:10 +0200

    Create main.c
    This is the first example from my 4th video on the series

---
+++ Lessons/Variables-Data-Types -io/main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void main()
+{
+	int x;
+	printf("Please write a number\n");
+	
+	scanf("%d", &x);
+	
+	printf("x is equal to %d \n", x);
+}
